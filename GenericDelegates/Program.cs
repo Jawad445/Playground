527dfc7c5f62de6ff44014c64c7b77526acd571d
Syed Jawad Hassan Naqvi <jhassan445@outlook.com>
2022-03-04 17:31:40 +0500

    generic delegate implemetation

---
+++ GenericDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿//Work with every type
+CalculateAndPrint("this", "that", (x, y) => $"{x} and {y}");
+CalculateAndPrint(20.5, 10.3, (x, y) => x + y);
+CalculateAndPrint(20, 10, (x, y) => x - y);
+CalculateAndPrint(true, false, (x, y) => x && y);
+
+Console.WriteLine("Hello World!");
+
+
+// Pass delegate into parameter
+static void CalculateAndPrint<T>(T x, T y, MathOp<T> f)
+{
+    var result = f(x, y);
+    Console.WriteLine(result);
+}
+
+//Generic Implementation of Delegate
+delegate T MathOp<T>(T a, T b);
