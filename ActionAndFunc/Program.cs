90224f0e73f0e36761e781e1ff6ed6d2bdfb5d96
Syed Jawad Hassan Naqvi <jhassan445@outlook.com>
2022-03-05 08:45:59 +0500

    Action and func implementation

---
+++ ActionAndFunc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+var Heros = new List<Hero>
+                    {
+                        new("wade", "william","Deadpool", false),
+                        new(string.Empty,string.Empty,"Superman",true),
+                        new("Bruce","wayen","Batman",false),
+                        new(string.Empty,string.Empty,"Homelander",true)
+                    };
+
+var result = Filter(Heros, h => h.canFly);
+
+var herosWhoCanFly = string.Join(',', result);
+
+Console.WriteLine(herosWhoCanFly);
+
+IEnumerable<T> Filter<T>(IEnumerable<T> items, Func<T, bool> f)
+{
+    foreach (var item in items)
+    {
+        if (f(item))
+        {
+            yield return item;
+        }
+    }
+}
+record Hero(string firstName, string lastName, string HeroName, bool canFly);
+
