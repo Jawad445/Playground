b70332f05550f64ef8af24795312d9f6b86f9f93
Syed Jawad Hassan Naqvi <jhassan445@outlook.com>
2022-03-07 15:48:31 +0500

    Linq start implemention

---
+++ Linq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var result = GenerateNumbers(10).Where(n => {
+                                        return n%2 == 0;
+                                    }).Select(n => {
+                                            return n * 3; 
+                                    });
+
+foreach(var item in result)
+{
+    Console.WriteLine(item);
+}
+
+//Good approach
+IEnumerable<int> GenerateNumbers(int maxval)
+{
+    for(int i=0; i <= maxval; i++)
+    {
+        yield return i;
+    }
+}
+
+
+//Bad way of writing code
+// IEnumerable<int> GenerateNumbers(int maxval)
+// {
+//     var result = new List<int>();
+//     for(int i=0; i <= maxval; i++)
+//     {
+//         result.Add(i);
+//     }
+
+//     return result;
+// }
