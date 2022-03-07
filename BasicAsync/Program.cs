3148119e1bf20a217f68339f4998d405c1a5c32e
Syed Jawad Hassan Naqvi <jhassan445@outlook.com>
2022-03-07 10:06:09 +0500

    basic async implementation

---
+++ BasicAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Tasks :
+    The Task class represents a single operation that does not return a value and that usually executes asynchronously.
+    Task object typically executes asynchronously on a thread pool thread rather than synchronously on the main application thread.
+*/
+using System.IO;
+
+//Old Way of writing syncrionus
+// Make sure main thread is not exit until task completed
+// File.ReadAllLinesAsync("data.txt")
+//             .ContinueWith(t => {
+//                 if(t.IsFaulted){
+//                     Console.Error.WriteLine(t.Exception);
+//                     return;
+//                 }
+//                     foreach(var item in  t.Result)
+//                     {
+//                         Console.WriteLine(item);
+//                     }            
+//             });
+
+//New way of writing async
+async Task ReadFile(){
+    var lines =  await File.ReadAllLinesAsync("data.txt");
+
+    foreach(var item in lines){
+        Console.WriteLine(item);
+    }
+}
+
+await ReadFile();
+
+Console.ReadKey();
